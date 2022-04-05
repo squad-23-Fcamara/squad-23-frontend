a132a4137a1f5d15f10f61b5dbeb42496223b7b9
Luiza Picoli <luzilio.p@gmail.com>
2022-04-05 16:42:45 -0300

    filter base for mentors' search creation

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// -- FILTER FOR MENTORS (for each character while writing) -- 
+
+const inputSearch = document.querySelector('');
+
+inputSearch.addEventListener('input', event => {
+    console.log(event.target.value.trim());
+})
+
+// -- GENERAL FUNCTION TO CREATE TAG W/ TEXTNODE  -- 
+
+function createElementWithText(tagName, text) {
+    const tag = document.createElement(tagName);
+    const textNode = document.createTextNode(text);
+
+    tag.appendChild(textNode);
+    return tag;
+} 
