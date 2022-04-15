f61a5e0d9c1e5586b9e50e47d580b22f1b29a3a8
JohnyCruz37 <102477755+JohnyCruz37@users.noreply.github.com>
2022-04-15 13:55:42 -0300

    finalized aside menu

--- scripts/aside.js
+++ scripts/aside.js
@@ -2,7 +2,6 @@ const menu = document.querySelector('.nav-menu');
 const navBtn = document.querySelector('.navBtn');
 
 navBtn.addEventListener('click', function(){
-   console.log(menu); 
-   menu.classList.add('nav-menu-open')
+   menu.classList.toggle('nav-menu-open')
 })
 
