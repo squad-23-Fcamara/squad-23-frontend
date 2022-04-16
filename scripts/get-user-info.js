03594ac460a0b3f3580caf1b50388e151795eb02
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-04-15 21:31:57 -0300

    Created script to get user info and show mentorings on perfil

--- scripts/get-user-info.js
+++ scripts/get-user-info.js
@@ -1,6 +1,43 @@
-const testerUUID = 'df6dafd4-42fe-4d55-bd72-934c6a602c89';
+const testerUUID = 'df6dafd4-42fe-4d55-bd72-934c6a602c89'
 
-let user 
+let user
+
+function userInfo() {
+  const container = document.getElementById('user-info')
+  const wrapper = document.createElement('div')
+  const userName = document.createElement('h2')
+  const userRole = document.createElement('h4')
+
+  userName.textContent = user.name
+  userRole.textContent = user.role
+
+  wrapper.appendChild(userName)
+  wrapper.appendChild(userRole)
+
+  container.appendChild(wrapper)
+}
+
+function userMentorings(schedule, id) {
+  const mentoringId = id
+  const mentoringsInfo = document.getElementById('mentorings')
+  const date = document.createElement('div')
+  const hour = document.createElement('div')
+
+  date.textContent = new Intl.DateTimeFormat('pt-BR', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  }).format(new Date(schedule))
+
+  hour.textContent = new Intl.DateTimeFormat('pt-BR', {
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZone: 'UTC'
+  }).format(new Date(schedule))
+
+  mentoringsInfo.appendChild(date)
+  mentoringsInfo.appendChild(hour)
+}
 
 async function fetchUser() {
   const response = await axios.get(
@@ -10,6 +47,9 @@ async function fetchUser() {
   user = response.data
 
   console.log(user)
+
+  userInfo()
+  user.mentorings.map(mentoring => userMentorings(mentoring.schedule_to))
 }
 
 fetchUser()
